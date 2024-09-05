48179c0d7f33b8826b966613286d60500c630fa4
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2024-09-05 12:16:12 +0200

    removed aw from eval

--- src/evaluation/evaluator.py
+++ src/evaluation/evaluator.py
@@ -8,7 +8,6 @@ from matplotlib.lines import Line2D
 from tqdm import tqdm
 
 # local imports
-from evaluation.alphawaves_data_extractor import AlphaWavesDataExtractor
 from evaluation.simulation_data_extractor import SimulationDataExtractor
 from evaluation.coherence_computer import CoherenceComputer
 from utils.plotting_setup import PLOT_SIZE, COLORS, notation, PLOT_FORMAT
@@ -17,49 +16,29 @@ from utils.paths import paths
 
 class Evaluator:
     """
-    A class responsible for evaluating simulated EEG data against real EEG data from the AlphaWaves dataset.
-    It computes and compares metrics such as coherence and power spectra across different brain regions (nodes).
+    A class responsible for evaluating simulated EEG data .
+    It computes metrics such as coherence and power spectra across different brain regions (nodes).
 
     Attributes
     ----------
     frequencies : list
         The frequency range for evaluating the data ([0, 30] Hz).
-    event : str
-        The event type for which the real data is extracted (eyes closed condition).
-    alpha_waves_data_extractor : AlphaWavesDataExtractor
-        An instance of the :py:class:`src.evaluation.alphawaves_data_extractor.AlphaWavesDataExtractor` class
-        used to extract and process real EEG data.
     simulation_data_extractor : SimulationDataExtractor
         An instance of the :py:class:`src.evaluation.simulation_data_extractor.SimulationDataExtractor` class
         used to extract and process simulated EEG data.
-    _real_avg_time_series : dict
-        A dictionary storing the average time series data for each brain region in the real data.
-    _real_frequencies : numpy.ndarray
-        The frequency values associated with the real power spectrum.
-    _real_powers : dict
-        A dictionary storing the power spectrum data for each brain region in the real data.
     """
 
     def __init__(self):
         """
-        Initializes the Evaluator class, setting up the frequency range, event type, and loading real and simulated data.
+        Initializes the Evaluator class, setting up the frequency range and loading real and simulated data.
         """
 
         self.frequencies = (0, 30)
-        self.event = "closed"
-
-        self.alpha_waves_data_extractor = AlphaWavesDataExtractor(fs=256, frequencies=self.frequencies)
         self.simulation_data_extractor = SimulationDataExtractor()
-
-        # real
-        self._real_avg_time_series = {}
-        self._real_frequencies = None
-        self._real_powers = {}
-        self._extract_real_data()
 
     def evaluate(self, plot_overview=True):
         """
-        Evaluates and compares the coherence and power metrics between simulated and real data
+        Evaluates and compares the coherence and power metrics
         using :py:meth:`_evaluate_metric`.
 
         Parameters
@@ -74,29 +53,6 @@ class Evaluator:
         self._evaluate_metric(self._evaluate_power_node, "Evaluating power", plot_overview, 2, 2, "Powers_summary")
 
         print(f"The evaluation plots have been saved in the 'plots' directory.")
-
-    def _extract_real_data(self):
-        """
-        Extracts the average time series and power spectrum
-        for each brain region (node) across all subjects from the AlphaWaves dataset using
-        :py:meth:`src.evaluation.alphawaves_data_extractor.AlphaWavesDataExtractor.get_time_series_per_subject_collection_event`.
-        """
-
-        for node in self.alpha_waves_data_extractor.channels_collections.keys():
-            self._real_avg_time_series[node] = []
-            self._real_powers[node] = []
-
-            for subject_id in range(len(self.alpha_waves_data_extractor.subjects)):
-                avg_time_series, f, avg_psd = self.alpha_waves_data_extractor.get_time_series_per_subject_collection_event(
-                    subject_id=subject_id,
-                    channels_collection=self.alpha_waves_data_extractor.channels_collections[node],
-                    event=self.event
-                )
-                self._real_avg_time_series[node].append(avg_time_series)
-                self._real_powers[node].append(avg_psd)
-
-                if self._real_frequencies is None:
-                    self._real_frequencies = f
 
     def _evaluate_metric(self, evaluation_func, desc, plot_overview, rows, cols, save_file_name):
         """
@@ -160,7 +116,7 @@ class Evaluator:
             A tuple containing one or two nodes, depending on the value of `pairwise`.
         """
 
-        nodes = list(self.alpha_waves_data_extractor.channels_collections.keys())
+        nodes = list(self.simulation_data_extractor.nodes[:-1])
         if pairwise:
             for i, node1 in enumerate(nodes):
                 for node2 in nodes[i + 1:]:
@@ -171,7 +127,7 @@ class Evaluator:
 
     def _evaluate_power_node(self, node, fig=None, ax=None, show_legend=True):
         """
-        Evaluates (using extracted real data :py:meth:`_get_simulated_power` for simulated data)
+        Evaluates (using :py:meth:`_get_simulated_power`)
         and plots (using :py:meth:`_plot_metric`) the power spectrum for a given node.
 
         Parameters
@@ -186,13 +142,10 @@ class Evaluator:
             If True, shows the legend on the plot (default is True).
         """
 
-        real_frequencies, real_powers = self._real_frequencies, self._real_powers[node]
-
         sim_frequencies, sim_powers = self._get_simulated_power(node)
 
         self._plot_metric(
             f"{notation(node)}",
-            real_frequencies, real_powers,
             sim_frequencies, sim_powers,
             fig=fig, ax=ax, show_legend=show_legend if ax is not None else True,
             y_label="Power", xlim=[self.frequencies[0], self.frequencies[1]], ylim=[0, 2e7], file_label=f"power_{node}"
@@ -200,8 +153,7 @@ class Evaluator:
 
     def _evaluate_coherence_node_pair(self, node1, node2, fig=None, ax=None, show_legend=True):
         """
-        Evaluates (using :py:meth:`_get_real_coherences_per_subject` for real data and
-        :py:meth:`_get_simulated_coherences` for simulated data)
+        Evaluates (using :py:meth:`_get_simulated_coherences`)
         and plots (using :py:meth:`_plot_metric`)
         the coherence between a pair of nodes.
 
@@ -219,18 +171,14 @@ class Evaluator:
             If True, shows the legend on the plot (default is True).
         """
 
-        # real eeg data
-        real_frequencies_coherence, real_coherences_per_subject = self._get_real_coherences_per_subject(node1, node2)
-
         # simulated data
         sim_frequencies_coherence, sim_coherences = self._get_simulated_coherences(node1, node2)
 
         self._plot_metric(
             f"{notation(node1)} — {notation(node2)}",
-            real_frequencies_coherence, real_coherences_per_subject,
             sim_frequencies_coherence, sim_coherences,
             fig=fig, ax=ax, show_legend=show_legend if ax is not None else True,
-            y_label="Coherence", xlim=[1, self.frequencies[1]], ylim=[0, 1], file_label=f"coherence_{node1}_{node2}"
+            y_label="Coherence", xlim=[1, self.frequencies[1]], ylim=[0, 0.6], file_label=f"coherence_{node1}_{node2}"
         )
 
     def _get_simulated_power(self, node):
@@ -293,57 +241,20 @@ class Evaluator:
 
         return freq_coh_sim, coherences
 
-    def _get_real_coherences_per_subject(self, node1, node2):
-        """
-        Computes the coherence between a pair of nodes across all subjects in the real dataset
-        using :py:meth:`src.simulation.coherence_computer.CoherenceComputer.compute_coherence_matched`.
-
-        Parameters
-        ----------
-        node1 : str
-            The name of the first brain region.
-        node2 : str
-            The name of the second brain region.
-
-        Returns
-        -------
-        tuple
-            A tuple containing:
-            - frequencies (numpy.ndarray): The array of frequencies for coherence.
-            - coherences (numpy.ndarray): An array of coherence values across all subjects.
-        """
-
-        coherence_computer_real = CoherenceComputer(fs=self.alpha_waves_data_extractor.fs)
-        coherences = []
-
-        for subject_id in range(len(self.alpha_waves_data_extractor.subjects)):
-            avg_time_series1 = self._real_avg_time_series[node1][subject_id]
-            avg_time_series2 = self._real_avg_time_series[node2][subject_id]
-
-            freq_coh_real, coh = coherence_computer_real.compute_coherence_matched(avg_time_series1, avg_time_series2)
-            coherences.append(coh)
-
-        return freq_coh_real, np.array(coherences)
-
     def _plot_metric(
             self,
             title,
-            real_frequencies, real_data,
             sim_frequencies, sim_data,
             fig=None, ax=None, show_legend=True, y_label=None, xlim=None, ylim=None, file_label=None
     ):
         """
-        Plots a comparison of a metric (e.g., coherence or power) between real and simulated data
-        using :py:meth:`_plot_real_data` and :py:meth:`_plot_simulated_data`.
+        Plots a metric (e.g., coherence or power) of data
+        using :py:meth:`_plot_simulated_data`.
 
         Parameters
         ----------
         title : str
             The title of the plot.
-        real_frequencies : numpy.ndarray
-            The array of frequencies for the real data.
-        real_data : numpy.ndarray
-            The real data (e.g., power or coherence) to plot.
         sim_frequencies : numpy.ndarray
             The array of frequencies for the simulated data.
         sim_data : dict
@@ -368,7 +279,6 @@ class Evaluator:
         if independent:
             fig, ax = plt.subplots(figsize=(PLOT_SIZE * 2, PLOT_SIZE))
 
-        self._plot_real_data(ax, real_frequencies, real_data)
         self._plot_simulated_data(ax, sim_frequencies, sim_data)
 
         ax.set_title(title)
@@ -379,42 +289,12 @@ class Evaluator:
 
         if show_legend:
             handles, labels = ax.get_legend_handles_labels()
-            handles = [Line2D([0], [0], color="000000", label="AW subjects", alpha=0.1)] + handles
             ax.legend(handles=handles, loc='upper right')
 
         if independent:
             # plt.show()
             path = paths.plots_path / f"{file_label}_{title}.{PLOT_FORMAT}"
             fig.savefig(path)
-
-    @staticmethod
-    def _plot_real_data(ax, frequencies, data):
-        """
-        Plots the real EEG data on a given axis.
-
-        Parameters
-        ----------
-        ax : matplotlib.axes.Axes
-            The axis object for plotting.
-        frequencies : numpy.ndarray
-            The array of frequencies for the real data.
-        data : numpy.ndarray
-            The real data (e.g., power or coherence) to plot.
-        """
-
-        for d in data:
-            ax.plot(frequencies, d, color="000000", alpha=0.02)
-        mean_data = np.mean(data, axis=0)
-        std_data = np.std(data, axis=0)
-        ax.fill_between(
-            frequencies,
-            mean_data - std_data,
-            mean_data + std_data,
-            color="000000",
-            alpha=0.05,
-            label="std AW subjects"
-        )
-        ax.plot(frequencies, mean_data, linestyle="dotted", label="mean AW subjects", color="000000", alpha=0.5)
 
     @staticmethod
     def _plot_simulated_data(ax, frequencies, data):
